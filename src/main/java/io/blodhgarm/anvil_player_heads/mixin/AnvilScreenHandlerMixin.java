ba4418032f2af28dc347323474ce8047a7398685
Dragon-Seeker <timekeeperguild@gmail.com>
2024-07-01 19:48:03 -0500

    Convert profile lookup to by async

--- src/main/java/io/blodhgarm/anvil_player_heads/mixin/AnvilScreenHandlerMixin.java
+++ src/main/java/io/blodhgarm/anvil_player_heads/mixin/AnvilScreenHandlerMixin.java
@@ -14,8 +14,6 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 @Mixin(AnvilScreenHandler.class)
 public abstract class AnvilScreenHandlerMixin extends ForgingScreenHandler {
 
-    @Shadow @Final private Property levelCost;
-
     @Shadow private String newItemName;
 
     public AnvilScreenHandlerMixin(@Nullable ScreenHandlerType<?> type, int syncId, PlayerInventory playerInventory, ScreenHandlerContext context) {
@@ -24,9 +22,6 @@ public abstract class AnvilScreenHandlerMixin extends ForgingScreenHandler {
 
     @Inject(at = @At("HEAD"), method = "updateResult", cancellable = true)
     private void init(CallbackInfo info) {
-        if (!AnvilPlayerHeads.handleHeadRename(this.player, this.slots, newItemName)) return;
-
-        levelCost.set(1);
-        info.cancel();
+        if (AnvilPlayerHeads.canHandleRenaming(this.player, this.slots, newItemName)) info.cancel();
     }
 }
